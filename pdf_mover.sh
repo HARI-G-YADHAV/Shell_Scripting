b9a02dd8e308ffda2d2f53d5de0a5faaa0b68a4b
HARI-G-YADHAV <ambadileelakal@gmail.com>
2023-03-26 00:38:24 +0530

    msg

--- pdf_mover.sh
+++ pdf_mover.sh
@@ -1,10 +1,6 @@
 #pdf move to folder
 #!/bin/bash
-<<<<<<< HEAD
-read -p "Enter path : "  p
-`cd /home/hari/Desktop/`
 
-=======
 # set the directory name to search for
 read -p "Enter directry name : "  dir_name
 cd ~
@@ -14,8 +10,8 @@ echo $dir_path
 cd $dir_path
 mkdir pdf
 ls
-for i in *.pdf
+for i in '*.pdf'
 do
 	mv $i pdf
 done
->>>>>>> cd96003d25c7ae7adb2dfdc17699b2b6ad252ce9
+
